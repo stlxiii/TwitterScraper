83dca5045fee5e15ad00a80a807307c036e58760
Patrick Pugh <pugh.patrick@outlook.com>
2021-03-08 08:19:30 -0500

    fix for pc platform

--- libraries/log.py
+++ libraries/log.py
@@ -25,7 +25,7 @@ class _log(object):
         if print_out:
             print(text)
 
-        with open(self.file, 'a') as f:
+        with open(self.file, 'a', encoding='utf-8') as f:
             if text == '':
                 f.write ('')
             else:
@@ -39,14 +39,17 @@ class _log(object):
         file = os.path.join(self.path, name)
         size = get_file_len(file) if compare_size else 0
 
-        if len(content) > size:
-            with open(file, 'w') as f:
-                f.write(content)
+        try:
+            if len(content) > size:
+                with open(file, 'w', encoding='utf8') as f:
+                    f.write(content)
+        except Exception as ex:
+            self.debug(f'Error saving file {name} : {ex}')
 
 
 def get_file_len(file):
     if os.path.exists(file) and os.path.isfile(file):
-        with open(file, 'r') as f:
+        with open(file, 'r', encoding='utf8') as f:
             return len(f.read())
     else:
         return 0
